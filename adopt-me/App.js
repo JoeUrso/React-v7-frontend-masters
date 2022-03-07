857dc4478cb0a5c409d8f400277a565b821ff818
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-03-07 14:07:12 -0800

    Add Pet function and 3 instances of it

--- adopt-me/App.js
+++ adopt-me/App.js
@@ -1,9 +1,30 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ]);
+};
+
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "Adopt Me!")
-    );
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Roxy",
+            animal: "Dog",
+            breed: "Cockapoo",
+        }),
+        React.createElement(Pet, {
+            name: "Slumpy",
+            animal: "Cat",
+            breed: "Ragdoll",
+        }),
+        React.createElement(Pet, {
+            name: "Jim",
+            animal: "Bird",
+            breed: "Parrot",
+        }),
+    ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
