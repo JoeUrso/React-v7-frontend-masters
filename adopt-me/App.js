58a5edd951a9fe19687c1a4ee34eac965ce0883f
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-03-07 15:10:07 -0800

    Install react and react-dom

--- adopt-me/App.js
+++ adopt-me/App.js
@@ -1,4 +1,7 @@
-const Pet = () => {
+import React from "react";
+import { render } from "react-dom";
+
+const Pet = (props) => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, props.name),
         React.createElement("h2", {}, props.animal),
@@ -27,4 +30,4 @@ const App = () => {
     ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
