0eebc8ae92265273f5bd627a8c6c45ecbab86de1
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-03-08 14:23:14 -0800

    Create Results component and update SearchParams accordingly

---
+++ adopt-me/Results.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Pet from "./Pet";
+
+export default function Results({ pets }) {
+    return (
+        <div>
+            {!pets.length ? (
+                <h1>No Pets Found</h1>
+            ) : (
+                pets.map((pet) => (
+                    <Pet
+                        name={pet.name}
+                        animal={pet.animal}
+                        breed={pet.breed}
+                        key={pet.id}
+                        images={pet.images}
+                        location={`${pet.city}, ${pet.state}`}
+                    />
+                ))
+            )}
+        </div>
+    );
+}
