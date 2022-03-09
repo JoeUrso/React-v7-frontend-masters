d8bb00824cbfae69c5c2892e5150581835470d56
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-03-08 17:33:46 -0800

    Create Details component

--- adopt-me/Details.js
+++ adopt-me/Details.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Details() {
-    return <div>Details</div>;
+export default class Details extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { loading: true };
+    }
+
+    render() {
+        if (this.state.loading) {
+            return <h2>loading...</h2>;
+        }
+
+        const { animal, breed, city, state, description, name } = this.state;
+
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>
+                        {animal} - {breed} - {city}, {state}
+                    </h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        );
+    }
 }
