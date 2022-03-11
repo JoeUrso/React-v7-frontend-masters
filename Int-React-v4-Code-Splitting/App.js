b3ee2bf174c3177a0a7c38119da05d4baf67f78c
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-03-10 19:44:14 -0800

    Code split App

--- Int-React-v4-Code-Splitting/App.js
+++ Int-React-v4-Code-Splitting/App.js
@@ -1,25 +1,32 @@
-import { StrictMode, useState } from "react";
+import { lazy, StrictMode, Suspense, useState } from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => {
+    import("./Details.js");
+});
+const SearchParams = lazy(() => {
+    import("./SearchParams.js");
+});
 
 const App = () => {
     const theme = useState("darkblue");
     return (
         <StrictMode>
-            <ThemeContext.Provider value={theme}>
-                <BrowserRouter>
-                    <header>
-                        <Link to="/">Adopt Me!</Link>
-                    </header>
-                    <Routes>
-                        <Route path="/details/:id" element={<Details />} />
-                        <Route path="/" element={<SearchParams />} />
-                    </Routes>
-                </BrowserRouter>
-            </ThemeContext.Provider>
+            <Suspense fallback={<h2>loading...</h2>}>
+                <ThemeContext.Provider value={theme}>
+                    <BrowserRouter>
+                        <header>
+                            <Link to="/">Adopt Me!</Link>
+                        </header>
+                        <Routes>
+                            <Route path="/details/:id" element={<Details />} />
+                            <Route path="/" element={<SearchParams />} />
+                        </Routes>
+                    </BrowserRouter>
+                </ThemeContext.Provider>
+            </Suspense>
         </StrictMode>
     );
 };
