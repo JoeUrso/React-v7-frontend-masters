3ce9929bd1bb9e43e0387b2e8bc295d4a67a7828
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-03-11 14:20:15 -0800

    Refactor ClientApp App and index for SSR

--- Int-React-v4-Server-Side-Rendering/App.js
+++ Int-React-v4-Server-Side-Rendering/App.js
@@ -1,6 +1,5 @@
 import { StrictMode, useState } from "react";
-import { render } from "react-dom";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
@@ -10,18 +9,16 @@ const App = () => {
     return (
         <StrictMode>
             <ThemeContext.Provider value={theme}>
-                <BrowserRouter>
-                    <header>
-                        <Link to="/">Adopt Me!</Link>
-                    </header>
-                    <Routes>
-                        <Route path="/details/:id" element={<Details />} />
-                        <Route path="/" element={<SearchParams />} />
-                    </Routes>
-                </BrowserRouter>
+                <header>
+                    <Link to="/">Adopt Me!</Link>
+                </header>
+                <Routes>
+                    <Route path="/details/:id" element={<Details />} />
+                    <Route path="/" element={<SearchParams />} />
+                </Routes>
             </ThemeContext.Provider>
         </StrictMode>
     );
 };
 
-render(<App />, document.getElementById("root"));
+export default App;
