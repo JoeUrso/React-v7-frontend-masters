1e972e125e2046b3125543bb8c846aaf476c5e4e
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-03-14 16:59:37 -0700

    Update Index with new micro-reducers

--- Int-React-v4-Redux/reducers/index.js
+++ Int-React-v4-Redux/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "redux";
+import animal from "./animal";
+import theme from "./breed";
 import location from "./location";
+import breed from "./theme";
 
 export default combineReducers({
     location,
+    breed,
+    animal,
+    theme,
 });
