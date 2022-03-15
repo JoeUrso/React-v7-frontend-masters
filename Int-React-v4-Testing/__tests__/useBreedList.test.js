21c3edc609f20c4f893cdf65fb205e91b6c409c7
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-03-15 14:45:42 -0700

    Refactor useBreedList.test using renderHook

--- Int-React-v4-Testing/__tests__/useBreedList.test.js
+++ Int-React-v4-Testing/__tests__/useBreedList.test.js
@@ -3,24 +3,14 @@
  */
 
 import { expect, test } from "@jest/globals";
-import { render } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
 import useBreedList from "../useBreedList.js";
 
-function getBreedList(animal) {
-    let list;
-
-    function TestComponent() {
-        list = useBreedList(animal);
-        return null;
-    }
-
-    render(<TestComponent />);
-
-    return list;
-}
-
 test("gives an empty list with no animal", async () => {
-    const [breedList, status] = getBreedList();
+    const { result } = renderHook(() => useBreedList(""));
+
+    const [breedList, status] = result.current;
+
     expect(breedList).toHaveLength(0);
     expect(status).toBe("unloaded");
 });
