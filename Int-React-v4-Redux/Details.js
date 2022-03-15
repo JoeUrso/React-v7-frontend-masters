6cd9181600d277db099d6fba91077f141a849bc7
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-03-14 17:59:54 -0700

    Refactor Details

--- Int-React-v4-Redux/Details.js
+++ Int-React-v4-Redux/Details.js
@@ -1,9 +1,9 @@
 import { Component } from "react";
+import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import Modal from "./Modal";
-import ThemeContext from "./ThemeContext";
 
 class Details extends Component {
     state = { loading: true, showModal: false };
@@ -42,16 +42,14 @@ class Details extends Component {
                 <div>
                     <h1>{name}</h1>
                     <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
-                    <ThemeContext.Consumer>
-                        {([theme]) => (
-                            <button
-                                style={{ backgroundColor: theme }}
-                                onClick={this.toggleModal}
-                            >
-                                Adopt {name}
-                            </button>
-                        )}
-                    </ThemeContext.Consumer>
+
+                    <button
+                        style={{ backgroundColor: this.props.theme }}
+                        onClick={this.toggleModal}
+                    >
+                        Adopt {name}
+                    </button>
+
                     <p>{description}</p>
                     {showModal ? (
                         <Modal>
@@ -72,11 +70,15 @@ class Details extends Component {
     }
 }
 
+const mapStateToProps = ({ theme }) => ({ theme });
+
+const ReduxWrappedDetails = connect(mapStateToProps)(Details);
+
 const WrappedDetails = () => {
     const params = useParams();
     return (
         <ErrorBoundary>
-            <Details params={params} />;
+            <ReduxWrappedDetails params={params} />;
         </ErrorBoundary>
     );
 };
